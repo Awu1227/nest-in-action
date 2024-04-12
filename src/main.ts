0022b16dd230b579ac0f4bdd31523cbe0376b19d
xuewuding <dingxw@sxj.com>
2024-04-12 17:39:17 +0800

    :heavy_plus_sign:swc add

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // const expressApp = await NestFactory.create<NestExpressApplication>(AppModule); app可使用express的方法
   await app.listen(3000);
 }
 bootstrap();
